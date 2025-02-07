3121465217fd3907a769fab7f03ef3d844d553ac
yuganshgarg01 <yuganshgarg01@gmail.com>
2025-02-07 10:56:25 +0530

    Create program6.js
    Input with HTTP module

---
+++ program6.js
@@ -0,0 +1,10 @@
+const http = require('http'), qs = require('querystring');
+http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    res.end(`<form method="POST"><input name="name" placeholder="Your Name" /><button>Submit</button></form>`);
+  } else {
+    let body = '';
+    req.on('data', chunk => body += chunk);
+    req.on('end', () => res.end(`<h1>Hello, ${qs.parse(body).name || 'Guest'}!</h1>`));
+  }
+}).listen(3000, () => console.log('Server running on http://localhost:3000'));
